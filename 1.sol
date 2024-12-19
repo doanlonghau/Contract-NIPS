679597747235fcf0bfe18d54e207bbca5643d29d
doanlonghau <150165084+doanlonghau@users.noreply.github.com>
2024-12-20 06:45:37 +0700

    Update 1.sol

--- 1.sol
+++ 1.sol
@@ -1,1 +1,47 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts-upgradeable/token/ERC721/ERC721Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+
+contract ERC721Example is Initializable, ERC721Upgradeable, OwnableUpgradeable {
+
+    uint256 private _nextTokenId;
+    string public _baseUri;
+    uint256 public mintPrice;
+
+    /// @custom:oz-upgrades-unsafe-allow constructor
+    constructor() {
+        _disableInitializers();
+    }
+
+    function initialize(address initialOwner, string memory name, string memory symbol, bytes calldata extendData, uint256 initialMintPrice) initializer public {
+        __ERC721_init(name, symbol);
+        __Ownable_init(initialOwner);
+        _initInfo(extendData);
+        mintPrice = initialMintPrice;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseUri;
+    }
+
+    function mint(address to) external payable {
+        require(msg.value >= mintPrice, "Insufficient funds to mint");
+        uint256 tokenId = ++_nextTokenId;
+        _safeMint(to, tokenId);
+    }
+
+    function _initInfo(bytes calldata extendData) internal {
+        _baseUri = abi.decode(extendData, (string));
+    }
+
+    function setMintPrice(uint256 newPrice) external onlyOwner {
+        mintPrice = newPrice;
+    }
+
+    function withdraw() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+}
